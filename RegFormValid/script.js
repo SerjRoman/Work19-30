a3955214591c9c153bafc2b79973d2abd760a9f9
Serj <sergeyroman26@gmail.com>
2023-02-16 21:24:10 +0200

    RegForm

---
+++ RegFormValid/script.js
@@ -0,0 +1,92 @@
+// 
+const form = document.querySelector(".reg");
+// 
+const bannedWords = [
+    '%',
+    '^',
+    '&',
+]
+// 
+form.addEventListener("submit", (event)=>{
+    // 
+    event.preventDefault();
+    // 
+    let inputs = document.querySelectorAll('.input');
+    // 
+    let divErrors = document.querySelectorAll('.error')
+    // 
+    for (let error of divErrors){
+        error.remove();
+    }
+    // 
+    let result = true;
+    // 
+    for (let input of inputs){
+        // 
+        if (validation(input) == false){
+            result = false;
+        }
+    }
+    // 
+    if (result == true ){
+        form.submit()
+}
+
+
+})
+// 
+function createError(text, elem){
+    // 
+    let divError = document.createElement('div');
+    // 
+    let pError = document.createElement('p');
+    // 
+    let imgError = document.createElement('img');
+    // 
+    divError.append(pError);
+    // 
+    divError.append(imgError);
+    // 
+    elem.after(divError);
+    // 
+    imgError.src = "error.svg";
+    // 
+    pError.innerHTML = text;
+    // 
+    divError.classList.add('error');
+}
+// 
+function validation(input) {
+    // 
+    if (input.value == '') {
+        createError(`Поле пустое`, input);
+        // 
+        return false;
+        // 
+    }
+    // 
+    if (input.id == 'username') {
+        // 
+        for (let symbol of input.value) {
+            // 
+            if (bannedWords.includes(symbol)) {
+                // 
+                createError(`Введен недопустимый символ: ${symbol}`, input);
+                // 
+                return false;
+            }
+        }
+    }
+    // 
+    else if (input.id == "email"){
+        // 
+        if (!input.value.includes("@")) {
+            // 
+            createError(`Нету @`, input);
+            // 
+            return false;
+        }
+    }
+    // 
+    return true
+}
